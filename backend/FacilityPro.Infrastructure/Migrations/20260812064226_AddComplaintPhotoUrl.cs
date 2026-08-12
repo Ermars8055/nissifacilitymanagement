0c2230c97912b3800609d1f7ca51787b639f56c7
Ermars8055 <vineshcastar711@gmail.com>
2026-08-12 12:16:24 +0530

    Feature: Add photo upload to complaints

---
+++ backend/FacilityPro.Infrastructure/Migrations/20260812064226_AddComplaintPhotoUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacilityPro.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddComplaintPhotoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Complaints",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Complaints");
+        }
+    }
+}
